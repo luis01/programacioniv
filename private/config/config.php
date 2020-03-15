c14dd7fe3953cc0a5bc78d5ffceaccd6033b7451
Luis Hernandez <sarco_luis@hotmail.com>
2020-03-15 12:07:20 -0600

    Creacion del formulario de alumnos, insercion de registros

--- private/config/config.php
+++ private/config/config.php
@@ -2,6 +2,6 @@
 /**
  * Importamos la clase principal de conexion
  */
-include('../Conexion/DB.php');
-$conexion = DB('localhost','root','','db_app_academica');
+include('../../Conexion/DB.php');
+$conexion = new DB('localhost','root','','db_app_academica');
 ?>
