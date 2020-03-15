e6b06d41d673e4d7075130bd99e988f1bd3e45dd
Luis Hernandez <sarco_luis@hotmail.com>
2020-03-15 14:09:59 -0600

    Mejoras y correcciones

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,8 @@ document.addEventListener("DOMContentLoaded",event=>{
     mostrarVista.addEventListener('click',e=>{
         e.stopPropagation();
 
-        let modulo = e.toElement.dataset.modulo;
+        let modulo = e.srcElement.dataset.modulo;
+        
         fetch('public/vistas/alumnos/alumnos.html').then( resp=>resp.text() ).then(resp=>{
             $(`#vistas-${modulo}`).innerHTML = resp;
             
