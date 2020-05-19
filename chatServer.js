f3cb25676db2211685a7319c305b7a66aa0dcd51
luis01 <sarco_luis@hotmail.com>
2020-05-18 21:32:37 -0600

    uso de mongo para el chat + el chatServer.js

--- chatServer.js
+++ chatServer.js
@@ -1,14 +1,27 @@
+//NOTA: Este archivo debe de ir en la carpeta de node -> C:\Program Files\nodejs
 var http = require('http').Server(),
     io   = require('socket.io')(http),
     MongoClient = require('mongodb').MongoClient,
-    url  = 'mongodb://localhost:27017';
+    url  = 'mongodb://localhost:27017',
+    dbName = 'chatPrueba';
     
 io.on('connection',socket=>{
-    socket.on('enviarMensaje',msg=>{
-        io.emit('recibirMensaje',msg);
+    socket.on('enviarMensaje',(msg)=>{
+        MongoClient.connect(url, (err,client)=>{
+            const db = client.db(dbName);
+            db.collection('chat').insert({'user':'Luis', 'msg':msg});
+            io.emit('recibirMensaje',msg);
+        });
+    });
+    socket.on('chatHistory',()=>{
+        MongoClient.connect(url, (err, client)=>{
+            const db = client.db(dbName);
+            db.collection('chat').find({}).toArray((err, msgs)=>{
+                io.emit('chatHistory',msgs);
+            }); 
+        });
     });
 });
-
 http.listen(3001,()=>{
     console.log('Escuchando peticiones por el puerto 3001, LISTO');
 });
