866889cb8c68d6b985d8837f96ff3aa249601ae5
luis01 <sarco_luis@hotmail.com>
2020-06-15 14:59:32 -0600

    Implementacion de las Notificaciones PUSH desde el Server

--- chatServer.js
+++ chatServer.js
@@ -4,13 +4,31 @@ var http = require('http').Server(),
     MongoClient = require('mongodb').MongoClient,
     url  = 'mongodb://localhost:27017',
     dbName = 'chatPrueba';
-    
+
+    const webpush = require('web-push'),
+        vapidKeys = {
+            publicKey:'BJHfdZQWnVUaHtWnSANmO46PAKlXzk6FnM2sYzSYxgjf-bNvNC8LHxwDrqi4Dt-LwbDyEB5K29sBX7PLRtOzB20',
+            privateKey:'QM1DCuNyxTIUSCtYbrT94WlbyREmfGVR0MVyj7_DUyQ'
+        };
+    var pushSubcriptions;//debe de almacenarse en una BD.
+    webpush.setVapidDetails("mailto:luishernandez@ugb.edu.sv",vapidKeys.publicKey, vapidKeys.privateKey);
+
 io.on('connection',socket=>{
     socket.on('enviarMensaje',(msg)=>{
         MongoClient.connect(url, (err,client)=>{
             const db = client.db(dbName);
             db.collection('chat').insert({'user':'Luis', 'msg':msg});
             io.emit('recibirMensaje',msg);
+            try {
+                const dataPush = JSON.stringify({
+                   title:'Notificacion PUSH desde el SERVIDOR',
+                   msg
+                });
+                console.log( "Endpoint: ", pushSubcriptions.endpoint );
+                webpush.sendNotification(pushSubcriptions,dataPush);
+            } catch (error) {
+                console.log("Error al intentar enviar la notificacion push", error);
+            }
         });
     });
     socket.on('chatHistory',()=>{
@@ -20,6 +38,10 @@ io.on('connection',socket=>{
                 io.emit('chatHistory',msgs);
             }); 
         });
+    });
+    socket.on("suscribirse",(subcriptions)=>{
+        pushSubcriptions = JSON.parse(subcriptions);
+        console.log( pushSubcriptions.endpoint );
     });
 });
 http.listen(3001,()=>{
